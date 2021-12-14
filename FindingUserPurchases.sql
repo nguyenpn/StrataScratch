718e5c24d7bec72da47c9b951959e33eeea7897b
nguyenpn <76017854+nguyenpn@users.noreply.github.com>
2021-12-13 21:48:23 -0800

    Create FindingUserPurchases.sql

---
+++ FindingUserPurchases.sql
@@ -0,0 +1,13 @@
+/*
+Write a query that'll identify returning active users. 
+A returning active user is a user that has made a second purchase
+within 7 days of any other of their purchases.
+Output a list of user_ids of these returning active users.
+*/
+
+SELECT DISTINCT a1.user_id
+  FROM amazon_transactions AS a1, amazon_transactions AS a2
+ WHERE a1.user_id = a2.user_id 
+   AND a1.created_at <> a2.created_at
+   AND a2.created_at - a1.created_at BETWEEN 0 AND 7
+ ORDER BY a1.user_id
