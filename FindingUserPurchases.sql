2b936aff78f3710c949199ed75288833e1f02af1
nguyenpn <76017854+nguyenpn@users.noreply.github.com>
2021-12-13 21:50:34 -0800

    Update FindingUserPurchases.sql

--- FindingUserPurchases.sql
+++ FindingUserPurchases.sql
@@ -8,6 +8,6 @@ Output a list of user_ids of these returning active users.
 SELECT DISTINCT a1.user_id
   FROM amazon_transactions AS a1, amazon_transactions AS a2
  WHERE a1.user_id = a2.user_id 
-   AND a1.created_at <> a2.created_at
+   AND a1.id <> a2.id
    AND a2.created_at - a1.created_at BETWEEN 0 AND 7
  ORDER BY a1.user_id
