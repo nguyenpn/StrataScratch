9ae72da53b2a822bd63bf49d862dcdcec5645643
nguyenpn <76017854+nguyenpn@users.noreply.github.com>
2021-12-13 22:07:09 -0800

    Update HighestEnergyConsumption.sql

--- HighestEnergyConsumption.sql
+++ HighestEnergyConsumption.sql
@@ -3,38 +3,29 @@ Find the date with the highest total energy consumption from the Facebook data c
 Output the date along with the total energy consumption across all data centers.
 */
 
-select c.date
-       ,max_consumption
-from (  select MAX(sum_consumption) AS max_consumption
-        from (  select date
-                      ,SUM(consumption) AS sum_consumption
-                from (
-                        select * from fb_eu_energy
-                        UNION all
-                        select * from fb_asia_energy
-                        UNION all
-                        select * from fb_na_energy
-                    ) as a
-                group by date
-            ) as c
-    ) as b
-    
-    JOIN 
-    
-    (   select date
-              ,SUM(consumption) AS sum_consumption
-        from (
-                select * from fb_eu_energy
-                UNION all
-                select * from fb_asia_energy
-                UNION all
-                select * from fb_na_energy
-            ) as a
-        group by date
-    ) as c
-    ON c.sum_consumption = b.max_consumption
+WITH
+all_cte AS (
+SELECT * FROM fb_eu_energy 
+UNION
+SELECT * FROM fb_asia_energy
+UNION 
+SELECT * FROM fb_na_energy
+)
 
+,sum_cons AS (
+SELECT date
+      ,SUM(consumption) AS sum_cons
+  FROM all_cte
+ GROUP BY date
+)
 
+,max_cons AS (
+SELECT MAX(sum_cons) AS max_cons 
+  FROM sum_cons
+)
 
-
-
+SELECT s.date
+       ,s.sum_cons
+  FROM max_cons AS m, sum_cons as s
+ WHERE m.max_cons = s.sum_cons
+ ORDER BY m.max_cons DESC
